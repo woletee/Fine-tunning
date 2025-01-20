f84dd00114fbc35473ee27a6f9aff8aa7c65eae4
Liyew <118590747+woletee@users.noreply.github.com>
2025-01-20 12:09:19 +0900

    Update Fine_tunning.py

--- Fine_tunning.py
+++ Fine_tunning.py
@@ -132,3 +132,4 @@ gpt2_model.resize_token_embeddings(len(gpt2_tokenizer))
 print("Fine-tuning for CNN, AlexNet, U-Net, ViT, BERT, and GPT-2.")
 #this repository is for fine-tunning cnn, alznet, transformer, and other models too
 #please start from here for fixing the remaining part so that It can be clear
+import numpy as np
